751ca93f4becd04bfa1e7733edce2ae1bb6ab5b7
Matthias Hahn <matthias.hahn@fiz-karlsruhe.de>
2011-08-12 15:32:08 +0200

    Initial checkin of Testplans

---
+++ getobject/runtest.sh
@@ -0,0 +1,4 @@
+#!/sbin/bash
+
+sh $JMETER_HOME/bin/jmeter -n -t FedoraGetObject.jmx -l logs/testlog.jtl -j logs/jmeter.log -q testplan.properties
+
---
+++ ingest/runtest.sh
@@ -0,0 +1,4 @@
+#!/sbin/bash
+
+sh $JMETER_HOME/bin/jmeter -n -t FedoraIngest.jmx -l logs/testlog.jtl -j logs/jmeter.log -q testplan.properties
+
