e0e0c33b52a72aaae5aa8664c4b4aba7112fc65a
Matthias Hahn <matthias.hahn@fiz-karlsruhe.de>
2011-08-19 16:16:12 +0200

    modified run.sh script

--- getobject/run.sh
+++ getobject/run.sh
@@ -7,7 +7,7 @@
 jmeter=`env | grep JMETER_HOME`
 
 if [ ${#jmeter} = 0 ]; then 
- echo "Please set JMETER_HOME"
+ echo "Please set JMETER_HOME to your installation directory"
  exit  
 fi
 
@@ -16,6 +16,8 @@ if [ -f testplan.properties ]; then
 url=`cat testplan.properties | grep authbaseurl | awk -F= {'print $2'}`
 user=`cat testplan.properties | grep authuser | awk -F= {'print $2'}`
 password=`cat testplan.properties | grep authpass | awk -F= {'print $2'}`
+marker=":"
+credentials=$user$marker$password
 else 
 
 echo "No testplan.properties available"
@@ -23,20 +25,26 @@ exit
 fi
 
 # spider the url
-returncode=`wget  --spider --user $user --password $password $url 2>&1 | grep  "200 OK" | awk {'print  $NF'}`
 
+returncode=`curl -IL --user $credentials $url 2>&1 | grep "HTTP/[0-9].[0-9] 200"`
+statuscode="1000"
+if [ ${#returncode} != 0 ]; then
+statuscode=`echo $returncode | awk {'print $2'}`
+else 
+ returncode="kein Status Code verfuegbar: Verbindungsaufbau fehlgeschlagen"
+fi
 
 # check if Status is OK 
 echo "checking Fedora Status"
-if [ ${#returncode} != 2 ]; then  
-       	echo $url "is not running! Goodbye. " 
- 	exit
-else
-	echo $url "is runnning."
+if [ $statuscode = "200" ]; then  
+       	echo $url "is runnning."
         # perform test
         echo "Starting Test..."
         sh runtest.sh	
-
+else
+	
+	echo $url "is not running! Goodbye. "
+        echo "HTTP Status code " $returncode " "
 fi
 
 
