2fb92d62f721bde5065bb2b6b8d446ab1e754b9c
Matthias Hahn <matthias.hahn@fiz-karlsruhe.de>
2011-08-26 14:14:03 +0200

    escidoc create item update - support for object information added

--- escidoc/create/item/runtest.sh
+++ escidoc/create/item/runtest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sh $JMETER_HOME/bin/jmeter -n -t escidocCreate.jmx -l logs/testlog.jtl -j logs/jmeter.log -q testplan.properties
+sh $JMETER_HOME/bin/jmeter -n -t escidocCreate.jmx -l logs/testlog.jtl -j logs/jmeter.log -q testplan.properties -p ../../properties/jmeter.properties
 
